c6facfd55dd4c35d37671d048f7dbf44ca9d5207
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-03-13 11:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+let output = document.getElementById("output");
+
+let array = [1,2,3,4];
+
+function arrayManipulation(array){
+	return new Promise((resolve,reject) =>{
+		setTimeout(()=>{
+			resolve(array);
+		},3000)
+	})
+	.then((array)=>{
+		return new Promise((resolve,reject)=>{
+		let solution = array.filter((a) => a%2==0)
+		setTimeout(()=>{
+				output.innerHTML = JSON.stringify(solution);
+			resolve(solution);
+		},1000);
+	})
+	})
+	.then((solution) =>{
+		let addition = solution.map((a) => a*2);
+		setTimeout(()=>{
+			output.innerHTML = JSON.stringify(addition);
+		},2000)
+	})
+	
+}
+ arrayManipulation(array);
